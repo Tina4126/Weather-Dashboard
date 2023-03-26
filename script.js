8606a3b70172888194fad8cbf27a4c086d678d48
smosafeer <sumayyah.mosafeer@hotmail.com>
2023-03-26 14:30:47 +0100

    javascript

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var APIKey = "a0b3eed94c9619ad95239c7e8f44cc29";
 
-var city = prompt("Enter city name"); 
+var city;
 
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
